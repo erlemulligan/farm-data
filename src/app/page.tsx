118565ddb4d03fbf727a629e9f0887c4c12d82b8
Erle Mulligan <erlemulligan@Erles-MacBook-Pro.local>
2024-02-21 11:50:11 -0600

    adding button component and dashboard page / tailwindcss config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import { Button } from '@/app/ui/Button'; 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1 className="text-4xl font-mono text-center">Farm Data Dashboard</h1>
+      <div className="z-10 max-w-5xl w-full text-sm">
+        <h1 className="text-6xl font-bold mb-8">Farm Dashboard</h1>
+        <Button href="https://www.usda.gov" target="_blank">
+          Go to USDA.gov
+        </Button>
       </div>
     </main>
   );
---
+++ src/app/ui/Button.tsx
@@ -0,0 +1,41 @@
+import React, { ReactNode } from 'react';
+
+type ButtonProps = {
+    type?: 'button' | 'submit' | 'reset';
+    href?: string;
+    target?: string;
+    onClick?: () => void;
+    children: ReactNode;
+    className?: string;
+};
+
+export const Button: React.FC<ButtonProps> = ({
+    type = 'button',
+    href,
+    target='_self',
+    onClick,
+    children,
+    className = 'bg-green-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full',
+}) => {
+    if (href) {
+        return (
+            <a href={href} target={target} className={className}>
+                {children}
+            </a>
+        );
+    }
+
+    if (onClick) {
+        return (
+            <button type={type} onClick={onClick} className={className}>
+                {children}
+            </button>
+        );
+    }
+
+    return (
+        <button type={type} className={className}>
+            {children}
+        </button>
+    );
+};
