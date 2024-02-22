5d8000305d87b55a41a63cfda4f3b0e6b2321f24
Erle Mulligan <erlemulligan@Erles-MacBook-Pro.local>
2024-02-22 14:54:32 -0600

    renaming directory and changing button target

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full text-sm">
         <h1 className="text-6xl font-bold mb-8">Farm Dashboard</h1>
-        <Button href="/usda" target="_blank">
+        <Button href="/usda" target="_self">
           Find USDA Data by Survey State
         </Button>
       </div>
