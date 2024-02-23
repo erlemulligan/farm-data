4b835ff4fd35865a412765b4f830b1dccb270171
Erle Mulligan <erlemulligan@Erles-MacBook-Pro.local>
2024-02-23 10:22:33 -0600

    adding data summary table for each state page

--- src/app/usda/[state]/page.tsx
+++ src/app/usda/[state]/page.tsx
@@ -1,7 +1,7 @@
 import { usePathname } from 'next/navigation';
 
 export const getUsdaStateData = async (state: string) => {
-    const res = await fetch(`https://api.ers.usda.gov/data/arms/surveydata?api_key=jKvoVGh59o0KwHwtqCLzXjHRbzGJ6JOAAInqpUkB&&year=2022&state=${state}&report&variable=igcfi`);
+    const res = await fetch(`https://api.ers.usda.gov/data/arms/surveydata?api_key=jKvoVGh59o0KwHwtqCLzXjHRbzGJ6JOAAInqpUkB&&year=2022&state=${state}&report=Farm%20Business%20Income%20Statement&variable=igcfi`);
     const data = await res.json();
 
     return data;
@@ -15,10 +15,38 @@ const UsdaState = async (props: any) => {
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="z-10 max-w-5xl w-full text-sm">
                 <h1 className="text-6xl font-bold mb-8">{state}</h1>
-                    <h2 className="text-2xl font-bold">Data</h2>
+                    {data ? (
+                    <><h2 className="text-2xl font-bold">Data Summary</h2><table className="w-full">
+                        <thead>
+                            <tr>
+                                <th>Year</th>
+                                <th>State</th>
+                                <th>Report</th>
+                                <th>Farm Type</th>
+                                <th>Estimate</th>
+                                <th>Median</th>
+                                <th>Statistic</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data.data.map((item: any) => (
+                                <tr key={item.id}>
+                                    <td>{item.year}</td>
+                                    <td>{item.state}</td>
+                                    <td>{item.report}</td>
+                                    <td>{item.farmtype}</td>
+                                    <td>{item.estimate}</td>
+                                    <td>{item.median}</td>
+                                    <td>{item.statistic}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table></>) : (
+                        <h2 className="text-2xl font-bold">No Data Found</h2>
+                    )}
 
                     {data && (
-                        <pre>{JSON.stringify(data, null, "\t")}</pre>
+                        <pre className='text-xs'>{JSON.stringify(data, null, "\t")}</pre>
                     )}
             </div>
         </main>
